147d7ac7eaa364700fc4710cd93f8a91d8cb7baa
gonghf <17791479837@163.com>
2020-09-10 14:13:47 +0800

    Add：RAR压缩示例

--- src/main/java/com/gonghf/file/compression/rar/RarRun.java
+++ src/main/java/com/gonghf/file/compression/rar/RarRun.java
@@ -1,4 +1,15 @@
 package com.gonghf.file.compression.rar;
 
+import java.io.IOException;
+
 public class RarRun {
+    public static void main(String[] args) {
+        String dir = "F:\\2k名单id.rar";
+        String out = "F:/test/tar";
+        try {
+            RarUtil.unRar(dir,out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/gonghf/file/compression/rar/RarUtil.java
+++ src/main/java/com/gonghf/file/compression/rar/RarUtil.java
@@ -1,4 +1,56 @@
 package com.gonghf.file.compression.rar;
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RarUtil {
+    public static String rar(String dir) {
+
+        return null;
+    }
+
+    /**
+     * RAR解压缩
+     *
+     * @param rarFilePath       rar文件路径
+     * @param deCompressionPath 解压到的目录
+     * @return
+     */
+    public static File[] unRar(String rarFilePath, String deCompressionPath) throws IOException {
+        File rarFile = new File(rarFilePath);
+        if (!rarFile.exists()) {
+            return null;
+        }
+        List<File> fileResults = new ArrayList<>();
+        File deCompression = new File(deCompressionPath);
+
+        FileInputStream inputStream = null;
+        ZipArchiveInputStream zipInputStream = null;
+
+        try {
+            inputStream = new FileInputStream(rarFile);
+            zipInputStream = new ZipArchiveInputStream(inputStream);
+            ZipArchiveEntry zipEntry = null;
+
+            while ((zipEntry = zipInputStream.getNextZipEntry()) != null){
+                System.out.println(zipEntry.getName());
+            }
+
+        } finally {
+            if (zipInputStream != null) {
+                zipInputStream.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        return fileResults.toArray(new File[fileResults.size()]);
+    }
 }
